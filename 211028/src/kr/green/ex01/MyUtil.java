32aa18d2971629abc24d4c9e727496720216a4cd
en13en57 <93195114+en13en57@users.noreply.github.com>
2021-12-07 22:26:16 +0900

    Delete 211028 directory

--- 211028/src/kr/green/ex01/MyUtil.java
+++
@@ -1,9 +0,0 @@
-package kr.green.ex01;
-
-public class MyUtil {
-	public static int KB = 1024;
-	
-	public static void getKB2Byte(int kb) {
-		System.out.println(kb + "KB는" + (kb*KB) + "Byte입니다.");
-	}
-}
