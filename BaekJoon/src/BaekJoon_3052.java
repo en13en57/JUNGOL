1caa4f8d65efc439a699e90c4b6ea94a9ee6f256
Doo O <en13en57@gmail.com>
2021-12-18 00:17:10 +0900

    BaekJoon

--- BaekJoon/src/BaekJoon_3052.java
+++ BaekJoon/src/BaekJoon_3052.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class Main {
+public class BaekJoon_3052 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		HashSet<Integer> h = new HashSet<Integer>();
