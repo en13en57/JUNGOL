4f716b653cca666125a286817fbf240ad1fc6442
Doo O <en13en57@gmail.com>
2021-12-19 22:54:23 +0900

    BaekJoon

--- BaekJoon/src/BaekJoon_1546.java
+++ BaekJoon/src/BaekJoon_1546.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {
+public class BaekJoon_1546 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
